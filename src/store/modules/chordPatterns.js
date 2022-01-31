b76c3f689b85921b33f936c61d4f51c7208d536a
Cory <corygreen84@hotmail.com>
2022-01-30 21:12:33 -0800

    code cleanup

--- src/store/modules/chordPatterns.js
+++ src/store/modules/chordPatterns.js
@@ -1,29 +1,4 @@
 const state = {
-    patterns: {
-        "patterns": {
-          "dyad": {
-            "Minor 2nd": [0, 1],
-            "Major 2nd": [0, 2],
-            "Minor 3rd": [0, 3],
-            "Major 3rd": [0, 4],
-            "Perfect 4th": [0, 5],
-            "Tritone": [0, 6],
-            "Perfect 5th": [0, 7],
-            "Minor 6th": [0, 8],
-            "Major 6th": [0, 9],
-            "Minor 7th": [0, 10],
-            "Major 7th": [0, 11],
-            "Perfect Octave": [0, 12]
-          },
-          "triads": {
-            
-          },
-          "sevenths": {
-            
-          },
-          "extended": {}
-        }
-    },
     allPatterns: {
         "Minor 2nd": [0, 1],
         "Major 2nd": [0, 2],
